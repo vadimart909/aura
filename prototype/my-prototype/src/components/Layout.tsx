16f3d7a38d6711abc4b90e37f2fef4d31c687b18
vadim_art <acidvadim@gmail.com>
2026-08-06 10:15:59 +0500

    fix: remove broken submodule reference prototype/design-system

--- prototype/my-prototype/src/components/Layout.tsx
+++ prototype/my-prototype/src/components/Layout.tsx
@@ -1,17 +1,5 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
-  return (
-    <>
-      <nav style={{ display: 'flex', gap: 'var(--spacing-4x)', padding: 'var(--spacing-4x)' }}>
-        <NavLink to="/" className="ts-400-m">
-          Главная
-        </NavLink>
-        <NavLink to="/about" className="ts-400-m">
-          О проекте
-        </NavLink>
-      </nav>
-      <Outlet />
-    </>
-  )
+  return <Outlet />
 }
